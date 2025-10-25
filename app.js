af36c273b02dda30207cdaa512ee9e0219c67fc2
Akshat Jain <gomat2230@gmail.com>
2025-10-25 16:37:28 +0530

    debug some code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import config from "./config.js";
 import { issueTokens, verifyAccessToken, rotateRefreshToken, revokeAll } from "./token.js";
-import { authMiddleware, requireHttps } from "./middlewares/auth.middleware.js";
+import { authMiddleware, requireHttps, authorizeRole } from "./middlewares/auth.middleware.js";
 
 export default config;
 export { issueTokens, verifyAccessToken, rotateRefreshToken, revokeAll };
-export { authMiddleware, requireHttps };
+export { authMiddleware, authorizeRole , requireHttps };
--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -38,6 +38,30 @@ export const authMiddleware = (options = {}) => {
   };
 };
 
+// auth/middlewares/authorizeRole.js
+
+export const authorizeRole = (...allowedRoles) => {
+  return (req, res, next) => {
+    try {
+      // req.user should already be set by verifyToken middleware
+      const user = req.user;
+
+      if (!user) {
+        return res.status(401).json({ message: "Unauthorized - No user found" });
+      }
+
+      if (!allowedRoles.includes(user.role)) {
+        return res.status(403).json({ message: "Access denied: Insufficient role" });
+      }
+
+      next();
+    } catch (error) {
+      res.status(500).json({ message: "Server error in role authorization", error });
+    }
+  };
+};
+
+
 export const requireHttps = (req, res, next) => {
   if (config.isProd) {
     const proto = req.headers["x-forwarded-proto"] || (req.protocol);
